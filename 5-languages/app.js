379323cb6aab77e9bc9aef68048f69cf5a2dbb0d
Loizwert <egor,89284157784@gmail.com>
2025-03-18 14:14:59 +0300

    Сделано задание 5-languages

---
+++ 5-languages/app.js
@@ -0,0 +1,15 @@
+const language = "AT";
+
+switch (language) {
+  case "RU":
+    console.log("Здравствуйте");
+    break;
+  case "EN":
+    console.log("Hello");
+    break;
+  case "DE":
+    console.log("Hallo");
+    break;
+  default:
+    console.log("Privet");
+}
